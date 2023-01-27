2678352387b2704bfab6510f6f7e4f99d02d9476
muratozkan79 <muratozkan79@gmail.com>
2023-01-28 02:11:20 +0300

    check city

--- app.js
+++ app.js
@@ -2,31 +2,57 @@ const divContainer = document.querySelector(".container");
 const searchButton = document.getElementById("button-addon2");
 const searchArea = document.getElementById("search-input");
 const cardContainer = document.querySelector(".cardContainer");
-
+let cityList = [];
 searchButton.addEventListener("click", function (e) {
   e.preventDefault();
-  const newCard = document.createElement("div");
-  newCard.classList.add(
-    "cards",
-    "col-2",
-    "bg-warning-subtle",
-    "border",
-    "rounded-pill",
-    "text=bobo"
-  );
-  cardContainer.append(newCard);
   var city = searchArea.value;
+  if (cityList.includes(city)) {
+    alert(
+      "You already know the weather for " +
+        city +
+        ". Please search another city"
+    );
+  } else {
+    cityList.push(city);
+    const newCard = document.createElement("div");
+    newCard.classList.add(
+      "cards",
+      "col-2",
+      "bg-warning-subtle",
+      "border",
+      "rounded-pill",
+      "text=bobo"
+    );
+    cardContainer.append(newCard);
 
-  fetch(`https://api.api-ninjas.com/v1/weather?city=${city}`, {
-    headers: { "X-Api-Key": "T6z75Z3C2s59SY6cTC1j7QKoIl6rKMcZbpAuPALQ" },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      const temp = data.temp;
-      const maxTemp = data.max_temp;
-      console.log(temp);
-      newCard.innerHTML =
-        "<h2 class='h5 pt-5 text-warning'>" + city.toUpperCase() + "<br>" + "<span class='h1'>" + temp + "°C" + "</span>" + "</h2>";
+    fetch(`https://api.api-ninjas.com/v1/weather?city=${city}`, {
+      headers: { "X-Api-Key": "T6z75Z3C2s59SY6cTC1j7QKoIl6rKMcZbpAuPALQ" },
     })
-    .catch((error) => console.log(error));
+      .then((response) => response.json())
+      .then((data) => {
+        const temp = data.temp;
+        const maxTemp = data.max_temp;
+        const minTemp = data.min_temp;
+        const humidity = data.humidity;
+        console.log(data);
+        newCard.innerHTML =
+          "<h2 class='h5 pt-4 text-warning'>" +
+          city.toUpperCase() +
+          "<br>" +
+          "<span class='h1'>" +
+          temp +
+          "°C" +
+          "</span>" +
+          "</h2><hr><h2 class='h5 pt-1 text-warning'>min/max " +
+          "<br>" +
+          minTemp +
+          "/" +
+          maxTemp +
+          " °C" +
+          "</h2><hr><h2 class='h5 pt-1 text-warning'>Humidity: " +
+          humidity +
+          "%</h2>";
+      })
+      .catch((error) => console.log(error));
+  }
 });
