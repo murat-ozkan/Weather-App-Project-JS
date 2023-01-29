fa27cef135f0d6d12ea81f2da6f90aa322f949bd
muratozkan79 <muratozkan79@gmail.com>
2023-01-29 14:52:55 +0300

    Some bugs fixed

--- app.js
+++ app.js
@@ -3,9 +3,15 @@ const searchButton = document.getElementById("button-addon2");
 const searchArea = document.getElementById("search-input");
 const cardContainer = document.querySelector(".cardContainer");
 let cityList = [];
+
+window.onload = function () {
+  document.getElementById("search-input").focus(); //* Focus the text area
+};
 searchButton.addEventListener("click", function (e) {
   e.preventDefault();
   var city = searchArea.value;
+  city = city.toUpperCase();
+  console.log(city);
   if (cityList.includes(city)) {
     alert(
       "You already know the weather for " +
@@ -13,31 +19,38 @@ searchButton.addEventListener("click", function (e) {
         ". Please search another city"
     );
   } else {
-    cityList.push(city);
-    const newCard = document.createElement("div");
-    newCard.classList.add(
-      "cards",
-      "col-2",
-      "bg-warning-subtle",
-      "border",
-      "rounded-pill",
-      "text=bobo"
-    );
-    cardContainer.append(newCard);
-
     fetch(`https://api.api-ninjas.com/v1/weather?city=${city}`, {
       headers: { "X-Api-Key": "T6z75Z3C2s59SY6cTC1j7QKoIl6rKMcZbpAuPALQ" },
     })
       .then((response) => response.json())
       .then((data) => {
-        const temp = data.temp;
+        //! veri gelirse çalışacak kısım
+        cityList.push(city);
+        const newCard = document.createElement("div");
+        newCard.classList.add(
+          "cards",
+          "col-3",
+          "bg-warning-subtle",
+          "border",
+          "border-warning",
+          "rounded-pill",
+          "p-4",
+          "shadow-lg"
+        );
+        if (cardContainer.firstChild) {
+          cardContainer.insertBefore(newCard, cardContainer.firstChild);
+        } else {
+          cardContainer.append(newCard);
+        } //! Buraya kadar cart ekleme
+        const temp = data.temp; //! Veri işleme burada başlar
         const maxTemp = data.max_temp;
         const minTemp = data.min_temp;
         const humidity = data.humidity;
         console.log(data);
+
         newCard.innerHTML =
           "<h2 class='h5 pt-4 text-warning'>" +
-          city.toUpperCase() +
+          city +
           "<br>" +
           "<span class='h1'>" +
           temp +
@@ -53,14 +66,17 @@ searchButton.addEventListener("click", function (e) {
           humidity +
           "%</h2>";
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        console.log(error.name);
+      });
   }
+  searchArea.value = "";
 });
 
 //! Proje Nasıl Geliştirilebilir?
-//* Try-Catch yapısı ile hata yakalama.
-//* Veri gelmediğinde Kart açılmaması için düzenleme.
-//* En son aranan şehri ilk başa tutturma.
-//* İlk açılışta input focus.
-//* Arama sonrası input reset.
+//* Try-Catch yapısı ile hata yakalama. OK
+//* Veri gelmediğinde Kart açılmaması için düzenleme. OK
+//* En son aranan şehri ilk başa tutturma. OK
+//* İlk açılışta input focus. OK
+//* Arama sonrası input reset. OK
 //* Kartlara hava durmuna uygun iconlar yerleştir
